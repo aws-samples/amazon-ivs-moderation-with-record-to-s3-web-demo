ae413249a816c829245ff0a501ca0115eea45f53
Christer Whitehorn <cjw@amazon.com>
2022-02-22 14:38:22 +1100

    set default region and Congnito auth type

--- web-ui/src/configs/config.js
+++ web-ui/src/configs/config.js
@@ -2,14 +2,14 @@ import Amplify from 'aws-amplify'
 
 export const awsConfig = {
   Auth: {
-    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
-    region: process.env.REACT_APP_REGION, 
-    userPoolId: process.env.REACT_APP_USER_POOL_ID, 
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
+    identityPoolId: "", 
+    userPoolId: "", 
+    userPoolWebClientId: "",
+    region: "us-west-2",
   },
-  aws_appsync_graphqlEndpoint: process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
-  apiGateway: process.env.REACT_APP_API_GATEWAY,
-  aws_appsync_authenticationType: process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
+  aws_appsync_graphqlEndpoint: "",
+  apiGateway: "",
+  aws_appsync_authenticationType: "AMAZON_COGNITO_USER_POOLS",
 }
 
 Amplify.configure(awsConfig)
